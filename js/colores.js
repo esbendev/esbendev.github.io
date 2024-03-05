bfe820ea779d6c359ceacb54dc1f14dbae2a4879
bensmithperez <bsmith@scnctech.com>
2024-03-04 16:46:45 -0800

    versión nueva!

---
+++ js/colores.js
@@ -0,0 +1,18 @@
+function initColores() {
+    if (localStorage.getItem('theme')) {
+        document.documentElement.setAttribute('data-theme', localStorage.getItem('theme'));
+    } else {
+        document.documentElement.setAttribute('data-theme', 'reset');
+        guardarColores();
+    }
+    // document.documentElement.setAttribute('data-theme', 'reset');
+}
+
+function guardarColores(botones,claseBoton, opcion) {
+    document.documentElement.setAttribute('data-theme', opcion);
+    localStorage.setItem('theme', document.documentElement.getAttribute('data-theme'));
+    for (let i = 0; i < botones.length; i++) {
+        botones[i].classList.remove('seleccionado');
+    }
+    claseBoton.classList.add('seleccionado');
+}
